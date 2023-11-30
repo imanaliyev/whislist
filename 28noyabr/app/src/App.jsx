1f4dd1e0aa41e233b7ab7ae87a9820cd6b49e82d
imanaliyev <mi357yzg6@code.edu.az>
2023-11-30 23:33:58 +0400

    salam

--- 28noyabr/app/src/App.jsx
+++ 28noyabr/app/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import './hook/useLocalStorage'
+import useLocalStorage from './hook/useLocalStorage';
 
 function App() {
   const [products, setProducts] = useState([])
-  const [whislist, setWhislist] = useState([])
+  const [whislist, setWhislist] = useLocalStorage("key",[])
+
   console.log(whislist);
 
 
@@ -33,18 +36,35 @@ function App() {
    }
 
   }
+
+  const removeItem =(item)=>{
+    setWhislist(whislist.filter(x=> x.id !== item.id))
+  }
+  
+ const wishlist = document.querySelector(".display-wishlist")
+  const handleTheme=()=>{
+    document.body.classList.toggle("dark")
+    wishlist.classList.toggle(".dark")
+   
+    
+   
+  }
+
   
  
   return (
     <div>
+      <button className='theme' onClick={handleTheme}>theme</button>
 
       <div className="wish-list">
+
       {whislist.map(x=><ul className='display-wishlist'>
           <img src={x.image} alt="" />
           <li>{x.id}</li>
           <li>{x.name}</li>
           <li>{x.description.slice(0,52)}</li>
           <div>{x.catagory}</div>
+          <button onClick={()=>removeItem(x)}>remove</button>
         </ul>)}
         
 
@@ -55,7 +75,6 @@ function App() {
         {products.map(x=><ul>
           <div onClick={()=>addWishLisht(x)} className='urek'><i class="fa-solid fa-heart"></i></div>
           <img src={x.image} alt="" />
-
           <li>{x.id}</li>
           <li>{x.name}</li>
           <li>{x.description.slice(0,52)}</li>
